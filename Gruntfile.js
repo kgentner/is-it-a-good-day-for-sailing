d417597583ac53cc2df96400fa486721ed4efbe9
kgentner <gentnerkarl@gmail.com>
2014-11-29 15:29:57 -0800

    added sailing_test to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,21 +7,25 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jshint: {
-      all: ['public/*.js', 'server.js'],
+      all: ['public/*.js', 'server.js', 'test/sailing_test.js', 'Gruntfile.js'],
       options: {
         jshintrc: true
       }
     },
 
     jscs: {
-      src: ['public/**/*.js', 'server.js'],
+      src: ['public/**/*.js', 'server.js', 'test/sailing_test.js',
+      'Gruntfile.js'],
       options: {
         config: '.jscsrc'
       }
     },
 
     simplemocha: {
-      src: ['test/**/*.js']
+      src: ['test/**/*.js'],
+      options: {
+        timeout: 3000
+      }
     }
   });
   grunt.registerTask('test', ['jshint', 'jscs', 'simplemocha']);
